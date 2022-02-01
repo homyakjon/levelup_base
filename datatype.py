52c630390de9757f0fd1997ee89a144ded032967
korol <ykorol@alkaloid.com.ua>
2022-02-01 22:49:14 +0200

    Home work 2

--- datatype.py
+++ datatype.py
@@ -4,8 +4,10 @@
 A = [1, 2, 3, 10, 20, 50, 65]
 i = 0
 sum = 0
-while i < len(A[i]):
+while i < len(A):
+    sum = sum + A[i]
     i += 1
+    break
     print('sum =', sum)
 
 a = float(input('input value a = '))
@@ -43,3 +45,35 @@ print(tuple(aria))
 my_list = (1, 2, 3, 4, 5)
 num = 6 in my_list
 print(num)
+
+# Home Work N 5
+
+from random import random
+a = int(random() * 100)
+b = int(random() * 100)
+if a%2 and b%2 or a%2==0 and b%2==0:
+    a -= 1
+print(a, b)
+if a%2:
+    print(a)
+else:
+    print(b)
+
+
+a, b, c = int(input()), int(input()), int(input())
+n = a
+if b > n:
+    n = b
+if c > n:
+    n = c
+    print(n)
+
+
+from random import choice
+numbers = [0, 1, 2, 'A', 3, 4, 'B', 6, 9, 'C', 'E', 5, 'T']
+def num(*args):
+    number = str(choice(args))
+    return number
+value = num(*numbers) + num(*numbers)
+print(f'Number: {value}')
+
